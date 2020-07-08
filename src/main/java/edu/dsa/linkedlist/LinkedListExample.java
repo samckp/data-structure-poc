9083a1348122d4ec4b2d8ab334e5bce80d9a9c45
samckp <sam.mca.liet@gmail.com>
2020-07-08 19:24:17 +0530

    deletion added

--- src/main/java/edu/dsa/linkedlist/LinkedListExample.java
+++ src/main/java/edu/dsa/linkedlist/LinkedListExample.java
@@ -18,24 +18,24 @@ public class LinkedListExample {
             head = new_node;
         }
 
-        public void insertAfter(Node prev_node, int new_data)
+        public void insertAfter(Node prev_node, int data)
         {
             if (prev_node == null)
             {
                 System.out.println("Previous node can't be null");
                 return;
             }
-            Node new_node = new Node(new_data);
+            Node new_node = new Node(data);
             new_node.next = prev_node.next;
             prev_node.next = new_node;
         }
 
-        public void append(int new_data)
+        public void append(int data)
         {
-            Node new_node = new Node(new_data);
+            Node new_node = new Node(data);
             if (head == null)
             {
-                head = new Node(new_data);
+                head = new Node(data);
                 return;
             }
             new_node.next = null;
@@ -56,8 +56,36 @@ public class LinkedListExample {
                 res_node = res_node.next;
             }
         }
+    public LinkedListExample deleteByKey(LinkedListExample list, int key)
+    {
+        // Hold header node
+        Node currNode = list.head;
+        Node prev = null;
 
-        public static void main(String[] args)
+        if (currNode != null && currNode.data == key) {
+            list.head = currNode.next;
+            System.out.println(key + " found and deleted");
+            return list;
+        }
+
+        while (currNode != null && currNode.data != key) {
+            prev = currNode;
+            currNode = currNode.next;
+        }
+
+        if (currNode != null) {
+            prev.next = currNode.next;
+            System.out.println(key + " found and deleted !!");
+        }
+
+        if (currNode == null) {
+            System.out.println(key + " not found!!");
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args)
         {
             LinkedListExample llist = new LinkedListExample();
 
@@ -69,6 +97,10 @@ public class LinkedListExample {
             llist.insertAfter(llist.head.next.next, 8);
 
             System.out.println("\n The Linked list is :: ");
+            llist.printList();
+
+            System.out.println("\n ------Delete by Key------");
+            llist.deleteByKey(llist, 4);
             llist.printList();
         }
     }
