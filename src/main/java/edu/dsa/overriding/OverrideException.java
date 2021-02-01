96db8ed6f175fa2955fef52f4f39333753d97f16
samckp <sam.mca.liet@gmail.com>
2021-02-01 11:38:36 +0530

    added

--- src/main/java/edu/dsa/overriding/OverrideException.java
+++ src/main/java/edu/dsa/overriding/OverrideException.java
@@ -1,2 +1,58 @@
-package edu.dsa.overriding;public class OverrideException {
+package edu.dsa.overriding;
+
+// Java program to demonstrate overriding when
+// superclass method does declare an exception
+
+    class Parent {
+        public Parent(){
+            System.out.println("Parent constructor!!");
+        }
+        void m1() throws RuntimeException
+        {
+            System.out.println("From parent m1()");
+        }
+    }
+
+    class Child1 extends Parent {
+        @Override
+            // no issue while throwing same exception
+        void m1() throws RuntimeException
+        {
+            System.out.println("From child1 m1()");
+        }
+    }
+    class Child2 extends Parent {
+        @Override
+            // no issue while throwing subclass exception
+        void m1() throws ArithmeticException
+        {
+            System.out.println("From child2 m1()");
+        }
+    }
+    class Child3 extends Parent {
+        @Override
+            // no issue while not throwing any exception
+        void m1()
+        {
+            System.out.println("From child3 m1()");
+        }
+    }
+    class Child4 extends Parent {
+        @Override
+            // compile-time error
+            // issue while throwing parent exception
+        void m1() //throws Exception
+        {
+            System.out.println("From child4 m1()");
+        }
+    }
+
+public class OverrideException {
+
+    public static void main(String[] args) {
+
+        Parent p = new Child3();
+        p.m1();
+    }
 }
+
